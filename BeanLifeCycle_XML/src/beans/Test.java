8a229a3071e7a263590fef414fc416da1438a563
499470 <499470@PC211204.cts.com>
2016-11-25 20:05:17 +0530

    Commited by Masroor

---
+++ BeanLifeCycle_XML/src/beans/Test.java
@@ -0,0 +1,65 @@
+package beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Test {
+	
+	private String driver, url, userName, password;
+	private Connection con;
+	
+	
+	
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void init() throws ClassNotFoundException, SQLException{
+		Class.forName(driver);
+		con= DriverManager.getConnection(url, userName, password);
+		
+		System.out.println("Connection to "+ con.getCatalog()+ " is successful");
+		
+	}
+	
+	public void save(int id, String name, String email, String address) throws SQLException {
+		PreparedStatement insertstmt=con.prepareStatement("insert into Employee values(?,?,?,?)");
+		insertstmt.setInt(1, id);
+		insertstmt.setString(2, name);
+		insertstmt.setString(3, email);
+		insertstmt.setString(4, address);
+		
+		insertstmt.executeUpdate();
+		
+		System.out.println("Insert statement executed");
+	}
+	
+	public void delete(int id) throws SQLException {
+		PreparedStatement deletestmt=con.prepareStatement("delete from employee where id=?");
+		deletestmt.setInt(1, id);
+		
+		deletestmt.executeUpdate();
+		
+		System.out.println("Deleted successfully");
+	}
+	
+	public void destroy() throws SQLException {
+		con.close();
+		System.out.println("Connection closed");
+	}
+
+}
---
+++ BeanLifeCycle_XML/src/test/Client.java
@@ -0,0 +1,61 @@
+package test;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import beans.Test;
+import beans.Test1;
+import beans.Test2;
+
+public class Client {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		//String file="resources/spring.xml";
+		String file="resources/spring-default-lifecycle.xml";
+		
+		ConfigurableApplicationContext cap = new ClassPathXmlApplicationContext(file);
+		
+		Test t= (Test)	cap.getBean("t");
+		
+		Test1 t1 = (Test1) cap.getBean("t1");
+		Test2 t2 = (Test2) cap.getBean("t2");
+		
+		Scanner sc= new Scanner(System.in);
+		
+		while(true){
+			System.out.println("Enter the choice\n1. Insert a new Record"
+					+ "\n2. Delete a Record\n3. Exit");
+			int ch= sc.nextInt();
+			
+			switch (ch) {
+			case 1:
+				System.out.println("Enter id");
+				int id=sc.nextInt();
+				System.out.println("Enter Name");
+				String name=sc.next();
+				System.out.println("Enter eamil id");
+				String email=sc.next();
+				System.out.println("Enter Address");
+				String address=sc.next();
+				t.save(id, name, email, address);
+				break;
+			
+			case 2:
+				System.out.println("Enter id");
+				int id1=sc.nextInt();
+				t.delete(id1);
+				break;
+			default:
+				cap.close();
+				break;
+			}
+			
+		}
+	
+		
+	}
+}
